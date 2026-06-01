4987c76a899449dd82e778909e9a230479217aa5
joaomartins06 <joaoferreirama@tudelft.nl>
2026-06-01 12:40:56 +0200

    corrections on kem and addition of  test_kem.py

--- kem/kem.py
+++ kem/kem.py
@@ -9,16 +9,16 @@ def encaps(pk, params: LWEParams = DEFAULT_PARAMS):
     #shared secret, binary string of length n
     mu = sample_uniform(q=2, size=(params.n,))
 
-    #generate random r, binary string of length m
-    r = sample_uniform(q=2, size=(params.m,))
+    #generate random matrix R, binary matrix m x n
+    R = sample_uniform(q=2, size=(params.m, params.n))
 
-    #u = A^T * r (mod q)
-    u = mod_matmul(pk[0].T, r, params.q)
+    #u = A^T * R (mod q), shape (n,n)
+    u = mod_matmul(pk[0].T, R, params.q)
 
-    #b^T * r (mod q)
-    bt_r_aux = mod_matmul(pk[1].T, r, params.q)
+    #b^T * R (mod q)
+    bt_r_aux = (pk[1] @ R) % params.q
 
-    #v = b^T r + mu * floor(q/2) mod q
+    #v = b^T R + mu * floor(q/2) mod q 
     v = mod_add(bt_r_aux, (params.q // 2) * mu, params.q)
 
     #compute the cyphertext (u, v)
@@ -31,15 +31,11 @@ def decaps(cyphertext, sk, params: LWEParams = DEFAULT_PARAMS):
 
     u, v = cyphertext
 
-    #w = v - s * u mod q
-    w = (v - sk * u) % params.q
+    #w = v - u * s mod q
+    w = (v - u.T @ sk) % params.q
     w_new = np.zeros(w.shape, dtype=np.uint8)
 
-    for i in range(len(w)):
-        #if w_i is closer to 0 than to q/2, set mu_i = 0, else mu_i = 1
-        if w[i] < params.q // 4 or w[i] > 3 * params.q // 4:
-            w_new[i] = 0
-        else:
-            w_new[i] = 1
+    #approximate w to 0 or 1 based on its value
+    w_new = ((w >= params.q // 4) & (w <= 3 * params.q // 4)).astype(np.uint8)
 
     return w_new
